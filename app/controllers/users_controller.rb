d6f0cd8004a53c60c5ee2c10e6a8f420a1dda452
Felix Reisinger <felix.reisinger@gmx.de>
2022-10-21 14:23:38 +0200

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
-end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
