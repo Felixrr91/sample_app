ef00f068562298684b91ac34de8407fb78517dc9
Felix Reisinger <felix.reisinger@gmx.de>
2022-10-21 14:50:38 +0200

    renwed controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
-    if @user.save
-    else
-      render 'new'
-    end
+  @user = User.new(user_params)
+  if @user.save
+    # Handle a successful save.
+  else
+    render 'new'
   end
+end
+
+private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
+end
